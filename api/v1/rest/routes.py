a7156a4ceebcebf72318e25adf95713ca54c1f73
Vladimir <x55aah@gmail.com>
2019-08-02 16:50:21 +0300

    update

--- api/v1/rest/routes.py
+++ api/v1/rest/routes.py
@@ -1,11 +1,11 @@
 # For more details about routing, see
 # http://www.tornadoweb.org/en/stable/routing.html
 from tornado.web import url
-from . import handlers
+from . import handlers as h
 
 
 route_patterns = [
-    url(r'^/report/(?P<id>[^/]+)/?$', handlers.ReportHandler, name='report'),
-    url(r'^/report/(?P<app_name>[^/]+)/(?P<app_version>[^/]+)/?$', handlers.ReportHandler, name='create_report'),
-    url(r'^/reports/?$', handlers.ReportListHandler, name='reports'),
+    url(r'^/report/(?P<id>[^/]+)/?$', h.ReportHandler, name='report'),
+    url(r'^/report/(?P<app_name>[^/]+)/(?P<app_version>[^/]+)/?$', h.ReportHandler, name='create_report'),
+    url(r'^/reports/?$', h.ReportListHandler, name='reports'),
 ]
